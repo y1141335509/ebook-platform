c7179c3cb105f7d29fed96d460711d97ad0f0856
y1141335509 <y1141335509@gmail.com>
2024-06-01 21:04:43 -0700

    first commit

---
+++ src/test/java/com/bubblesandbooks/ebooksplatform/EbooksPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bubblesandbooks.ebooksplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbooksPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
